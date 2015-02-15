70b9b470584488deac8452cce36fbb323c611ff2
ilopezluna <ignasi.lopez.luna@gmail.com>
2015-02-15 17:05:07 +0100

    - Added spring-boot-starter-actuator - Example of counter and gauge metrics

--- src/main/java/com/ilopezluna/services/TeamService.java
+++ src/main/java/com/ilopezluna/services/TeamService.java
@@ -3,6 +3,8 @@ package com.ilopezluna.services;
 import com.ilopezluna.entities.Team;
 import com.ilopezluna.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +18,22 @@ public class TeamService {
     @Autowired
     private TeamRepository teamRepository;
 
+    @Autowired
+    private CounterService counterService;
+
+    @Autowired
+    private GaugeService gaugeService;
+
     public Iterable<Team> findAll() {
-        return teamRepository.findAll();
+
+        long init = System.currentTimeMillis();
+        counterService.increment("com.ilopezluna.services.TeamService.findAll().counter");
+
+        Iterable<Team> all = teamRepository.findAll();
+        long elapsed = System.currentTimeMillis() - init;
+
+        gaugeService.submit("com.ilopezluna.services.TeamService.findAll().gauge", elapsed);
+
+        return all;
     }
 }
